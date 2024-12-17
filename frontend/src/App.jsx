037c90c6d8f30a07a6ec4cda5ca877f6888dbc7c
akshayybhat <akshayybhat6@gmail.com>
2024-12-17 17:16:39 +0530

    added signup route at Ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
+const SignupPage = React.lazy(() => import("./pages/SignupPage.jsx"));
 function App() {
 
   return (
     <div>
         Hello world
+        <BrowserRouter>
+            <Routes>
+                <Route path="/signup" element={<SignupPage/>}/>
+            </Routes>
+        </BrowserRouter>
     </div>
   )
 }
