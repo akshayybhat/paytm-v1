0de58fd3ea6138970394e554837a00b01d833bb1
akshayybhat <akshayybhat6@gmail.com>
2024-12-21 19:16:04 +0530

    added dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignupPage from "./pages/SignupPage.jsx";
 import SigninPage from "./pages/SigninPage.jsx";
+import DashboardPage from "./pages/DashboardPage.jsx";
 export default function App (){
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/signup" element={<SignupPage />}/>
                 <Route path="/signin" element={<SigninPage />}/>
+                <Route path="/dashboard" element={<DashboardPage />}/>
             </Routes>
         </BrowserRouter>
     )
---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,8 @@
+import Appbar from "../components/AppbarComponent.jsx";
+export default function DashboardPage(){
+    return(
+        <div>
+            <Appbar/>
+        </div>
+    );
+}
